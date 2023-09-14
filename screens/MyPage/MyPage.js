fedf2a55983f83ebe4a1cb6559bcd45b1f6d02c5
joohee <2170095@ewhain.net>
2023-09-14 16:01:09 +0900

    delete api 주석처리

--- screens/MyPage/MyPage.js
+++ screens/MyPage/MyPage.js
@@ -62,7 +62,7 @@ export default function MyPage({ navigation }) {
         text: "네",
         onPress: () => {
           console.log(deleteId);
-          deleteTeam(deleteId);
+          // deleteTeam(deleteId);
         },
       },
     ]);
