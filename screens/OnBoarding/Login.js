3ced81a901c7ec262c1b1f7bac542d701cc79217
NeoSelf1 <neoself1105@gmail.com>
2023-09-27 19:10:13 +0900

    socialLoginDone

--- screens/OnBoarding/Login.js
+++ screens/OnBoarding/Login.js
@@ -4,31 +4,22 @@ import axios from "axios";
 import { styled } from "styled-components/native";
 import * as KakaoLogin from "@react-native-seoul/kakao-login";
 import NaverLogin from "@react-native-seoul/naver-login";
-// import * as AppleAuthentication from "expo-apple-authentication";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
+import {
+  appleAuth,
+  AppleButton,
+} from "@invertase/react-native-apple-authentication";
 
 WebBrowser.maybeCompleteAuthSession();
 
+//Apple 소셜로그인 위해 언젠가는 사용해야할것
+//let user = null;
+
 export default function Login({ navigation }) {
   const [successNaver, setSuccessRes] = useState();
-  const [appleAuthAvailable, setAppleAuthAvailable] = useState(false);
   const [failureNaver, setFailureRes] = useState();
   const [getProfileRes, setGetProfileRes] = useState();
-
-  //   const appleLogin = async () => {
-  //     try {
-  //       const credential = await AppleAuthentication.signInAsync({
-  //         requestedScopes: [
-  //           AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-  //           AppleAuthentication.AppleAuthenticationScope.EMAIL,
-  //         ],
-  //       });
-  //       console.log(credential);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
 
   const [GoogleReq, GoogleRes, GooglepromptAsync] = Google.useAuthRequest({
     expoClientId:
@@ -41,30 +32,6 @@ export default function Login({ navigation }) {
       "317985927887-jk1lb4tj27lvvb750v2pfs6ud7k1doaa.apps.googleusercontent.com",
   });
 
-  const getTokenKakao = async (accessToken) => {
-    let url = "http://52.78.52.47:8080/";
-    let detailAPI = `oauth/KAKAO?accessToken=${accessToken}`; //500
-    //google에서 발급받는 동일한 AccessToken 집어 넣어도 + 같은 호출문 구조 사용해도 [AxiosError: Request failed with status code 500] 발생
-    const response = await axios.get(url + detailAPI, {
-      headers: {
-        "Content-Type": `application/json`,
-      },
-    });
-    const result = response.data;
-    return result;
-  };
-  const getTokenNaver = async (accessToken) => {
-    let url = "http://52.78.52.47:8080/";
-    let detailAPI = `oauth/NAVER?accessToken=${accessToken}`; //500
-    //google에서 발급받는 동일한 AccessToken 집어 넣어도 + 같은 호출문 구조 사용해도 [AxiosError: Request failed with status code 500] 발생
-    const response = await axios.get(url + detailAPI, {
-      headers: {
-        "Content-Type": `application/json`,
-      },
-    });
-    const result = response.data;
-    return result;
-  };
   const loginKakao = () => {
     KakaoLogin.login()
       .then((result) => {
@@ -84,7 +51,18 @@ export default function Login({ navigation }) {
         }
       });
   };
-
+  const getTokenKakao = async (accessToken) => {
+    let url = "http://52.78.52.47:8080/";
+    let detailAPI = `oauth/KAKAO?accessToken=${accessToken}`; //500
+    //google에서 발급받는 동일한 AccessToken 집어 넣어도 + 같은 호출문 구조 사용해도 [AxiosError: Request failed with status code 500] 발생
+    const response = await axios.get(url + detailAPI, {
+      headers: {
+        "Content-Type": `application/json`,
+      },
+    });
+    const result = response.data;
+    return result;
+  };
   const loginGoogle = async (accessToken) => {
     try {
       let url = "http://52.78.52.47:8080/";
@@ -118,7 +96,30 @@ export default function Login({ navigation }) {
       console.error("Failed to fetch data:", error);
     }
   };
-
+  const getTokenNaver = async (accessToken) => {
+    let url = "http://52.78.52.47:8080/";
+    let detailAPI = `oauth/NAVER?accessToken=${accessToken}`; //500
+    //google에서 발급받는 동일한 AccessToken 집어 넣어도 + 같은 호출문 구조 사용해도 [AxiosError: Request failed with status code 500] 발생
+    const response = await axios.get(url + detailAPI, {
+      headers: {
+        "Content-Type": `application/json`,
+      },
+    });
+    const result = response.data;
+    return result;
+  };
+  const getTokenApple = async (accessToken) => {
+    let url = "http://52.78.52.47:8080/";
+    let detailAPI = `oauth/APPLE?accessToken=${accessToken}`; //500
+    //google에서 발급받는 동일한 AccessToken 집어 넣어도 + 같은 호출문 구조 사용해도 [AxiosError: Request failed with status code 500] 발생
+    const response = await axios.get(url + detailAPI, {
+      headers: {
+        "Content-Type": `application/json`,
+      },
+    });
+    const result = response.data;
+    return result;
+  };
   const logoutNaver = async () => {
     console.log("Naver Logout");
     try {
@@ -130,36 +131,51 @@ export default function Login({ navigation }) {
       console.error(e);
     }
   };
-
-  const getProfile = async () => {
+  async function onAppleButtonPress() {
+    // performs login request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      // Note: it appears putting FULL_NAME first is important, see issue #293
+      requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+    });
+    // console.log("appleAuthRequestResponse", appleAuthRequestResponse);
+    const {
+      user: newUser,
+      email,
+      nonce,
+      identityToken,
+      realUserStatus /* etc */,
+    } = appleAuthRequestResponse;
     try {
-      const profileResult = await NaverLogin.getProfile(
-        successNaver?.accessToken
+      getTokenApple(identityToken).then((res) =>
+        console.log("AppleLogin API실행결과??:", res)
       );
-      setGetProfileRes(profileResult);
-    } catch (e) {
-      setGetProfileRes(undefined);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
     }
-  };
 
-  //   const getAppleAuthContent = () => {
-  //     return (
-  //       <AppleAuthentication.AppleAuthenticationButton
-  //         buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-  //         buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
-  //         cornerRadius={5}
-  //         style={{ width: 240, height: 64 }}
-  //         onPress={() => appleLogin()}
-  //       />
-  //     );
-  //   };
+    // get current authentication state for user
+    // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+    const credentialState = await appleAuth.getCredentialStateForUser(
+      appleAuthRequestResponse.user
+    );
+
+    // use credentialState response to ensure the user is authenticated
+    if (credentialState === appleAuth.State.AUTHORIZED) {
+      // user is authenticated
+    }
+  }
 
   useEffect(() => {
-    // const checkAvailable = async () => {
-    //   const isAvailable = await AppleAuthentication.isAvailableAsync();
-    //   setAppleAuthAvailable(isAvailable);
-    // };
-    // checkAvailable();
+    // onCredentialRevoked returns a function that will remove the event listener. useEffect will call this function when the component unmounts
+    return appleAuth.onCredentialRevoked(async () => {
+      console.warn(
+        "If this function executes, User Credentials have been Revoked"
+      );
+    });
+  }, []); // passing in an empty array as the second argument ensures this is only ran once when component mounts initially.
+
+  useEffect(() => {
     if (GoogleRes) {
       if (GoogleRes?.type === "success") {
         loginGoogle(GoogleRes.authentication.accessToken).then((result) =>
@@ -182,11 +198,18 @@ export default function Login({ navigation }) {
       <Button onPress={() => loginKakao()}>
         <ButtonText>Kakao로 시작하기</ButtonText>
       </Button>
-
       <Button onPress={() => logoutNaver()}>
         <ButtonText>Naver 로그아웃</ButtonText>
       </Button>
-      {appleAuthAvailable && getAppleAuthContent()}
+      <AppleButton
+        buttonStyle={AppleButton.Style.WHITE}
+        buttonType={AppleButton.Type.SIGN_IN}
+        style={{
+          width: 160, // You must specify a width
+          height: 45, // You must specify a height
+        }}
+        onPress={() => onAppleButtonPress()}
+      />
     </Container>
   );
 }
