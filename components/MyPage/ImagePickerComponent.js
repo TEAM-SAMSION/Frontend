ca47646a5935091d8c7097051d27aa44770fd58e
HIHJH <2170095@ewhain.net>
2023-09-29 04:44:15 +0900

    imagepicker수정

--- components/MyPage/ImagePickerComponent.js
+++ components/MyPage/ImagePickerComponent.js
@@ -12,7 +12,7 @@ import { AccessTokenAtom } from "../../recoil/AuthAtom";
 const ACCESSTOKEN =
   "eyJhbGciOiJIUzM4NCJ9.eyJ0b2tlbl90eXBlIjoiQUNDRVNTX1RPS0VOIiwiZW1haWwiOiJ0ZXN0IiwiaXNzIjoicGF3aXRoIiwiaWF0IjoxNjk1ODk1ODc2LCJleHAiOjE2OTU5ODIyNzZ9.i58lt1IYpj9YHOK1QZ3v3U0jjplv5SR4rkbQyM_qwCT3Tt2rbmVxi0U3BNuAUOcV";
 
-const ImagePickerComponent = (props) => {
+export const ImagePickerComponent = (props) => {
   const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
 
   const uploadImage = async () => {
