ab778a0e54995aa6bfc20d36c751bf206214eef9
황주희 <hwangjoohee@hwangjooheeui-MacBookAir.local>
2023-09-28 20:18:01 +0900

    add recoil

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import OnBoardingNav from "./navigators/OnBoardingNav";
 import LoggedInNav from "./navigators/LoggedInNav";
+import { RecoilRoot } from "recoil";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <LoggedInNav />
-      {/* <OnBoardingNav /> */}
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <LoggedInNav />
+        {/* <OnBoardingNav /> */}
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
---
+++ recoil/AuthAtom.js
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+export const AccessTokenAtom = atom({
+  key: "AccessTokenAtom",
+  default:
+    "eyJhbGciOiJIUzM4NCJ9.eyJ0b2tlbl90eXBlIjoiQUNDRVNTX1RPS0VOIiwiZW1haWwiOiJ0ZXN0IiwiaXNzIjoicGF3aXRoIiwiaWF0IjoxNjk1ODk1ODc2LCJleHAiOjE2OTU5ODIyNzZ9.i58lt1IYpj9YHOK1QZ3v3U0jjplv5SR4rkbQyM_qwCT3Tt2rbmVxi0U3BNuAUOcV",
+});
