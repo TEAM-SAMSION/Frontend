c48365fe76e6099fdfed2f1b4802d8906e83c502
HIHJH <2170095@ewhain.net>
2023-10-16 15:40:12 +0900

    팀 가입, 참여 페이지 생성

---
+++ screens/Home/CreateTeam.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function CreateTeam() {
+  return (
+    <View>
+      <Text>create</Text>
+    </View>
+  )
+}
---
+++ screens/Home/JoinTeam.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function JoinTeam() {
+  return (
+    <View>
+      <Text>create</Text>
+    </View>
+  )
+}
