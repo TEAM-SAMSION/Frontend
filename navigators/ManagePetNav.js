7545e084013a092d9c2bb65b81994c146362a11a
HIHJH <2170095@ewhain.net>
2023-11-15 02:55:35 +0900

    Admin 펫 관리 기능

--- navigators/ManagePetNav.js
+++ navigators/ManagePetNav.js
@@ -4,6 +4,8 @@ import ManagePet from '../screens/Administrator/ManagePet'
 import { colors } from '../colors'
 import { TouchableOpacity } from 'react-native'
 import BackButton from '../assets/Svgs/chevron_back.svg'
+import AddPet from '../screens/Administrator/AddPet'
+import EditPet from '../screens/Administrator/EditPet'
 
 const Stack = createStackNavigator()
 
@@ -26,6 +28,44 @@ export default function ManagePetNav() {
         component={ManagePet}
         options={({ navigation }) => ({
           headerTitle: '펫 관리',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginLeft: 16,
+              }}
+            >
+              <BackButton width={24} height={24} />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="AddPet"
+        component={AddPet}
+        options={({ navigation }) => ({
+          headerTitle: '펫 등록',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginLeft: 16,
+              }}
+            >
+              <BackButton width={24} height={24} />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="EditPet"
+        component={EditPet}
+        options={({ navigation }) => ({
+          headerTitle: '펫 정보 수정',
           headerLeft: () => (
             <TouchableOpacity
               onPress={() => navigation.goBack()}
