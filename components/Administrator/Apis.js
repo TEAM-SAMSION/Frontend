bbdbc86a13dca4d8218299dfbc2621eb41daf03a
NeoSelf1 <neoself1105@gmail.com>
2023-11-20 21:00:54 +0900

    axiosDone

--- components/Administrator/Apis.js
+++ components/Administrator/Apis.js
@@ -1,23 +1,16 @@
-import axios from 'axios'
 import { url } from '../Shared'
-
 ////// 팀 정보 //////
 export const getTeamInfo = async (accessToken, teamId) => {
   let API = `/teams/${teamId}`
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data
 }
 
 export const postTeamInfo = async (accessToken, teamId, data) => {
   let API = `/teams/${teamId}`
-  const response = await axios.post(url + API, data, {
+  const response = await axiosInstance.post(url + API, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
-      Authorization: accessToken,
     },
   })
 }
@@ -25,71 +18,48 @@ export const postTeamInfo = async (accessToken, teamId, data) => {
 ////// 회원 관리 //////
 export const getMember = async (accessToken, teamId) => {
   let API = `/teams/${teamId}/registers/manage`
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.content
 }
 
 export const changeAuthority = async (accessToken, teamId, registerId, authority) => {
   let API = `/teams/${teamId}/registers/${registerId}`
   let data = { authority: authority }
-  const response = await axios.put(url + API, data, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.put(url + API, data)
 }
 
 export const searchMember = async (accessToken, teamId, nickname) => {
   let API = `/teams/${teamId}/registers/search`
-  const response = await axios.get(url + API, {
-    params: { nickname: nickname },
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.content
 }
 
 ////// 펫 관리 //////
 export const getPet = async (accessToken, teamId) => {
   let API = `/teams/${teamId}/pets`
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.content
 }
 
 export const addPet = async (accessToken, teamId, data) => {
   let API = `/teams/${teamId}/pets`
-  const response = await axios.post(url + API, data, {
+  const response = await axiosInstance.post(url + API, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
-      Authorization: accessToken,
     },
   })
 }
 
 export const deletePet = async (accessToken, petId) => {
   let API = `/teams/pets/${petId}`
-  const response = await axios.delete(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.delete(url + API)
 }
 
 export const changePetInfo = async (accessToken, petId, data) => {
   let API = `/teams/pets/${petId}`
-  const response = await axios.post(url + API, data, {
+  const response = await axiosInstance.post(url + API, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
-      Authorization: accessToken,
     },
   })
 }
--- components/Home/Apis.js
+++ components/Home/Apis.js
@@ -1,10 +1,9 @@
-import axios from 'axios'
 import { url } from '../Shared'
-
+import axiosInstance from '../../utils/customAxios'
 //////////////////////// main 화면 ////////////////////////
 export const getUserInfo = async (accessToken) => {
   let API = `/user`
-  const response = await axios.get(url + API, {
+  const response = await axiosInstance.get(url + API, {
     headers: { Authorization: accessToken },
   })
   return response.data
@@ -12,43 +11,33 @@ export const getUserInfo = async (accessToken) => {
 
 export const getTodoProgress = async (accessToken, teamId) => {
   let API = `/teams/${teamId}/todos/progress`
-  const response = await axios.get(url + API, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.progress
 }
 
 export const getTeamList = async (accessToken) => {
   let API = `/teams/name`
-  const response = await axios.get(url + API, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.content
 }
 
 export const getMyTodoList = async (accessToken, teamId) => {
   let API = `/teams/${teamId}/todos`
-  const response = await axios.get(url + API, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.content
 }
 
 export const completeTodo = async (accessToken, todoId) => {
   let API = `/teams/todos/${todoId}/assign/complete`
   let body = {}
-  const response = await axios.put(url + API, body, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.put(url + API, body)
 }
 
 //////////////////////// 팀 참여 ////////////////////////
 
 export const getSearchedTeam = async (accessToken, teamCode) => {
   let API = `/teams/codes/${teamCode}`
-  const response = await axios.get(url + API, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.get(url + API)
   console.log(response.data)
   return response.data
 }
@@ -56,27 +45,22 @@ export const getSearchedTeam = async (accessToken, teamCode) => {
 export const postJoiningTeam = async (accessToken, teamCode) => {
   let API = `/teams/registers?todoTeamCode=${teamCode}`
   let body = {}
-  const response = await axios.post(url + API, body, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.post(url + API, body)
 }
 //////////////////////// 팀 생성 ////////////////////////
 
 export const getTeamCode = async (accessToken) => {
   let API = `/teams/codes/random`
-  const response = await axios.get(url + API, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.randomCode
 }
 
 export const postTeamInfo = async (accessToken, data) => {
   let API = `/teams`
-  const response = await axios.post(url + API, data, {
+  const response = await axiosInstance.post(url + API, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
       //Accept: 'application/json',
-      Authorization: accessToken,
     },
   })
 }
