bec97a0e6cfdba634095a88edcfc5397ab1b42f5
HIHJH <2170095@ewhain.net>
2023-11-20 21:05:48 +0900

    mypage axios 수정

--- components/MyPage/Apis.js
+++ components/MyPage/Apis.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 import { url } from '../Shared'
+import axiosInstance from '../../utils/customAxios'
 
 export const getUserInfo = async (accessToken) => {
   let API = `/user`
-  const response = await axios.get(url + API, {
-    headers: { Authorization: accessToken },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data
 }
 
@@ -13,9 +12,8 @@ export const getTeamList = async (accessToken) => {
   const page = 0
   const size = 10000
   let API = `/teams?page=${page}&size=${size}`
-  const response = await axios.get(url + API, {
+  const response = await axiosInstance.get(url + API, {
     params: { page, size },
-    headers: { Authorization: accessToken },
   })
   return response.data.content
 }
@@ -23,10 +21,9 @@ export const getTeamList = async (accessToken) => {
 /////// 정보 수정 //////
 export const changeProfileImage = async (accessToken, routeData) => {
   let API = '/user'
-  const response = await axios.post(url + API, routeData, {
+  const response = await axiosInstance.post(url + API, routeData, {
     headers: {
       'Content-Type': `multipart/form-data`,
-      Authorization: accessToken,
     },
   })
 }
@@ -34,34 +31,23 @@ export const changeProfileImage = async (accessToken, routeData) => {
 export const changeNickname = async (accessToken, nickName) => {
   let API = '/user/name'
   let data = { nickname: nickName }
-  const response = await axios.put(url + API, data, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.put(url + API, data)
 }
 
 /////// 팀 탈퇴 //////
 export const deleteTeam = async (accessToken, teamId) => {
   let API = `/teams/${teamId}/registers`
   let data = {}
-  const response = await axios.delete(url + API, data, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.delete(url + API, data)
   console.log(`${teamId} 삭제`)
 }
 
 export const getAllTodoList = async (accessToken, teamId, page) => {
   let API = `/teams/${teamId}/todos/withdraw`
-  const response = await axios.get(url + API, {
+  const response = await axiosInstance.get(url + API, {
     params: {
       page: page,
       size: 10,
-    },
-    headers: {
-      Authorization: accessToken,
     },
   })
   return response.data.content
@@ -69,34 +55,23 @@ export const getAllTodoList = async (accessToken, teamId, page) => {
 
 export const getTodoNum = async (accessToken, teamId) => {
   let API = `/teams/${teamId}/todos/withdraw/count`
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.todoCount
 }
 
 ////// 계정 탈퇴 //////
 export const getAllTeams = async (accessToken) => {
   let API = `/teams/withdraw`
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.content
 }
 
 export const getAllTodos = async (accessToken, page) => {
   let API = `/teams/todos/withdraw?page=${page}&size=10`
-  const response = await axios.get(url + API, {
+  const response = await axiosInstance.get(url + API, {
     params: {
       page: page,
       size: 10,
-    },
-    headers: {
-      Authorization: accessToken,
     },
   })
   return response.data.content
@@ -104,40 +79,24 @@ export const getAllTodos = async (accessToken, page) => {
 
 export const getAllTodoNum = async (accessToken) => {
   let API = `/teams/todos/withdraw/count`
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.todoCount
 }
 
 export const deleteAccount = async (accessToken) => {
   let API = `/user`
-  const response = await axios.delete(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.delete(url + API)
   console.log('탈퇴')
 }
 
 export const postReason = async (accessToken, data) => {
   let API = '/user/withdraw'
   let reason = { reason: data }
-  const response = await axios.post(url + API, reason, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.post(url + API, reason)
 }
 
 export const countDate = async (accessToken) => {
   let API = '/user/term'
-  const response = await axios.get(url + API, {
-    headers: {
-      Authorization: accessToken,
-    },
-  })
+  const response = await axiosInstance.get(url + API)
   return response.data.joinTerm
 }
