2ff8e29620d636b6e48a93d332655eb6f07dd3bb
HIHJH <2170095@ewhain.net>
2023-11-29 15:28:20 +0900

    fix: 폰트 크기 일정하게 시도

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { RecoilRoot } from 'recoil'
 import AppBase from './AppBase'
-import { LogBox } from 'react-native'
+import { LogBox, Text } from 'react-native'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import messaging from '@react-native-firebase/messaging'
 import { registerRootComponent } from 'expo'
+
+Text.defaultProps = Text.defaultProps || {}
+Text.defaultProps.allowFontScaling = false
 
 LogBox.ignoreAllLogs()
 messaging().setBackgroundMessageHandler(async (message) => {
