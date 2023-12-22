92e0077464d0ceec708c042e7ed240521def994d
NeoSelf1 <neoself1105@gmail.com>
2023-12-23 00:59:16 +0900

    test

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -16,7 +16,7 @@ import { SelectedTeamAtom, TabBarAtom } from '../../recoil/TabAtom'
 import Swiper from 'react-native-swiper'
 import RightIcon from '../../assets/Svgs/chevron_right.svg'
 import { ChristmasModal } from './ChristmasModal'
-import { checkVersionTemp } from '../../utils/VersionControl'
+import { checkVersion } from '../../utils/VersionControl'
 
 export default function Home({ navigation }) {
   const isFocused = useIsFocused()
@@ -102,7 +102,7 @@ export default function Home({ navigation }) {
     }
   }
   useEffect(() => {
-    checkVersionTemp()
+    checkVersion()
     getUserNickname()
     fetchData()
   }, [])
